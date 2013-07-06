karma-runner/karma-mocha

commit 9bdcae24f98cdcee51ed0fb76dd8043c9ce443ed

grep tests if options.grep

src/adapter.js
@@ -80,6 +80,9 @@ var createMochaReporterConstructor = function(tc) {
 
 var createMochaStartFn = function(mocha) {
   return function(config) {
+    if(config.grep){
+      mocha.grep(config.grep);
+    }
     mocha.run();
   };
 };

commit edfa5345c2ea879c4e30924440d6f7fd7f9ea58e

fix: config does not have to be defined
This is a fix for bug introduced in 9bdcae24f98cdcee51ed0fb76dd8043c9ce443ed

src/adapter.js
@@ -80,7 +80,7 @@ var createMochaReporterConstructor = function(tc) {
 
 var createMochaStartFn = function(mocha) {
   return function(config) {
-    if(config.grep){
+    if(config && config.grep){
       mocha.grep(config.grep);
     }
     mocha.run();