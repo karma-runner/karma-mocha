898d4efba47ccc2dfc208dd72cb18cac7a448327
Keith Cirkel <keithamus@github.com>
2017-10-03 15:11:05 +0100

    feat: allow `mocha.run()` to be opted out of with `autoRun=false`

--- src/adapter.js
+++ src/adapter.js
@@ -205,6 +205,10 @@ var createMochaStartFn = function (mocha) {
       if (clientArguments.grep) {
         mocha.grep(clientArguments.grep)
       }
+
+      if (clientArguments.autoRun === false) {
+        return
+      }
     }
 
     mocha.run()
